b6ae6c08594071dca1415d44db64080544de6f6f
Noah Levin <noah.levin.coding@gmail.com>
2024-08-01 18:51:35 +0200

    Add 30 fps video

--- Network Guardians TD 2/scripts/ui/titlescreen_video.gd
+++ Network Guardians TD 2/scripts/ui/titlescreen_video.gd
@@ -1,14 +1,23 @@
 extends Node2D
 
 @onready var video_stream_player = $VideoStreamPlayer
+@onready var label = $Label
 
-func _process(delta):
-	print(delta)
+@export var video_60fps : String
+@export var video_30fps : String
+
+
 	
+func _ready():
+	label.text = OS.get_name()
 	
-func _input(event):
-	if Input.is_action_just_pressed("left_mouse_button"):
-		video_stream_player.visible = false
-		video_stream_player.paused = true
+	if OS.get_name() == "Web":
+		video_stream_player.stream.file = video_30fps
+		print("30fps")
+	else:
+		video_stream_player.stream.file = video_60fps
+		print("60fps")
+		
+	video_stream_player.play()
 
 
