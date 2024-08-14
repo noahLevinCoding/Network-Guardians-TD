da192b289459f778eef567a8a2e1a3d752ae159f
Noah Levin <noah.levin.coding@gmail.com>
2024-08-14 15:04:04 +0200

    Change attack tower resource order

--- Network Guardians TD 2/scripts/tower/attack_tower_resource.gd
+++ Network Guardians TD 2/scripts/tower/attack_tower_resource.gd
@@ -4,24 +4,26 @@ extends TowerResource
 @export var name : String
 
 @export_group("Stats")
+@export var damage_type : DAMAGE_TYPE 
+@export var attack_damage : float
 @export var attack_speed : float
 @export var attack_range : float
-@export var attack_damage : float
 @export var pierce : int = 1
-@export var bullet_speed : float = 1000
-@export var bullet_effect : BulletEffectResource = null
+@export var number_of_targets : int = 1
 @export var power : int = 0
 @export var temperature_increase : float = 0
-@export var number_of_targets : int = 1
-@export var damage_type : DAMAGE_TYPE 
-@export var extra_damage_to_trojan : bool = false
 
-@export_group("Effects")
+@export_group("Bullet Stats")
+@export var bullet_speed : float = 1000
+@export var bullet_effect : BulletEffectResource = null
+
+@export_group("Enemy Effects")
 @export var effects : Array[EnemyEffectResource]
 
 @export_group("Visibility and vulnerability")
 @export var ignores_damage_type_immunity : bool = false
 @export var can_see_camo : bool = false
+@export var extra_damage_to_trojan : bool = false
 
 @export_group("Visuals")
 @export var sprite_frames : SpriteFrames
@@ -29,21 +31,27 @@ extends TowerResource
 @export var bullet_visual_resource : BulletVisualResource
 
 @export_group("Upgrades")
+
+@export_subgroup("Upgrade 1")
 @export var upgrade_path_1_icon : Texture2D
-@export var upgrade_path_2_icon : Texture2D
 
 @export var upgrade_path_1_description : String
-@export var upgrade_path_2_description : String
 
 @export var upgrade_path_1_price_easy : int = 0
 @export var upgrade_path_1_price_medium : int = 0
 @export var upgrade_path_1_price_hard : int = 0
 
+@export var upgrade_path_1_tower_resource : TowerResource
+
+@export_subgroup("Upgrade 2")
+@export var upgrade_path_2_icon : Texture2D
+
+@export var upgrade_path_2_description : String
+
 @export var upgrade_path_2_price_easy : int = 0
 @export var upgrade_path_2_price_medium : int = 0
 @export var upgrade_path_2_price_hard : int = 0
 
-@export var upgrade_path_1_tower_resource : TowerResource
 @export var upgrade_path_2_tower_resource : TowerResource
 
 
