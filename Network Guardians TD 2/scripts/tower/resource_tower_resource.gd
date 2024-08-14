0c740a8a819601a06482669e0f8130820b4190c9
Noah Levin <noah.levin.coding@gmail.com>
2024-08-14 15:05:46 +0200

    Same with resource and support tower

--- Network Guardians TD 2/scripts/tower/resource_tower_resource.gd
+++ Network Guardians TD 2/scripts/tower/resource_tower_resource.gd
@@ -2,6 +2,8 @@ class_name ResourceTowerResource
 extends TowerResource
 
 @export var name : String
+
+@export_group("Stats")
 @export var drop_time : float
 @export var drop_amount : int
 @export var max_drop_amount : int
@@ -13,19 +15,26 @@ extends TowerResource
 @export var place_col_shape : Shape2D
 
 @export_group("Upgrades")
+
+@export_subgroup("Upgrade 1")
 @export var upgrade_path_1_icon : Texture2D
-@export var upgrade_path_2_icon : Texture2D
 
 @export var upgrade_path_1_description : String
-@export var upgrade_path_2_description : String
 
 @export var upgrade_path_1_price_easy : int = 0
 @export var upgrade_path_1_price_medium : int = 0
 @export var upgrade_path_1_price_hard : int = 0
 
+@export var upgrade_path_1_tower_resource : TowerResource
+
+@export_subgroup("Upgrade 2")
+@export var upgrade_path_2_icon : Texture2D
+
+@export var upgrade_path_2_description : String
+
 @export var upgrade_path_2_price_easy : int = 0
 @export var upgrade_path_2_price_medium : int = 0
 @export var upgrade_path_2_price_hard : int = 0
 
-@export var upgrade_path_1_tower_resource : TowerResource
 @export var upgrade_path_2_tower_resource : TowerResource
+
--- Network Guardians TD 2/scripts/tower/support_tower_resource.gd
+++ Network Guardians TD 2/scripts/tower/support_tower_resource.gd
@@ -8,7 +8,7 @@ extends TowerResource
 @export var power : int = 0
 @export var temperature_increase : float = 0
 
-@export_group("Effects")
+@export_group("Tower Effects")
 @export var effects : Array[TowerEffectResource]
 
 
@@ -17,20 +17,26 @@ extends TowerResource
 @export var place_col_shape : Shape2D
 
 @export_group("Upgrades")
+
+@export_subgroup("Upgrade 1")
 @export var upgrade_path_1_icon : Texture2D
-@export var upgrade_path_2_icon : Texture2D
 
 @export var upgrade_path_1_description : String
-@export var upgrade_path_2_description : String
 
 @export var upgrade_path_1_price_easy : int = 0
 @export var upgrade_path_1_price_medium : int = 0
 @export var upgrade_path_1_price_hard : int = 0
 
+@export var upgrade_path_1_tower_resource : TowerResource
+
+@export_subgroup("Upgrade 2")
+@export var upgrade_path_2_icon : Texture2D
+
+@export var upgrade_path_2_description : String
+
 @export var upgrade_path_2_price_easy : int = 0
 @export var upgrade_path_2_price_medium : int = 0
 @export var upgrade_path_2_price_hard : int = 0
 
-@export var upgrade_path_1_tower_resource : TowerResource
 @export var upgrade_path_2_tower_resource : TowerResource
 
